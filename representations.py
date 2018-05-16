7cb82aab7d5e837f12095fda37a00a03d6d69728
Soham Chattopadhyay <ch.soham@gmail.com>
2018-05-16 17:03:10 -0500

    Tests-debugged-16-May-2018-passed

--- representations.py
+++ representations.py
@@ -22,25 +22,24 @@ from collections import namedtuple
 
 
 class dumbbell(namedtuple('dumbbell','i o R c')):
-    
+
     def __eq__(self,other):
         true_class = isinstance(other,self.__class__)
-        if (self.i==other.i and np.allclose(self.R,other.R,atol=1e-8) and np.allclose(self.o,other.o,atol=1e-8) 
-            and self.c==other.c):
+        if (self.i==other.i and np.allclose(self.R,other.R) and np.allclose(self.o,other.o) and self.c==other.c):
             return (True and true_class)
     def __ne__(self,other):
-        return not self.__eq__(other) 
-    
+        return not self.__eq__(other)
+
     def __add__(self,other):
         if not isinstance(other,jump):
             raise TypeError("Can only add a jump to a state.")
-            
-        if not (self.i==other.i and np.allclose(self.o,other.o1,atol=1e-8)
-                and np.allclose(self.R,other.R_1,atol=1e-8) and self.c==other.c1): 
+
+        if not (self.i==other.i and np.allclose(self.o,other.or1,atol=1e-8)
+                and np.allclose(self.R,other.R1,atol=1e-8) and self.c==other.c1):
             raise ArithmeticError("Initial state of Jump object must match current dumbbell state")
-        
-        return self.__class__(other.j,other.o2,other.R2,other.c2)
-        
+
+        return self.__class__(other.j,other.or2,other.R2,other.c2)
+
     def gop(self,crys,chem,g):
         r1, (ch,i1) = crys.g_pos(g,self.R,(chem,self.i))
         o1 = np.dot(g.cartrot,self.o)
@@ -57,36 +56,37 @@ class dumbbell(namedtuple('dumbbell','i o R c')):
 # In[10]:
 
 
-class pairs(namedtuple('pairs',"i_s R_s db")):
-     
+class SdPair(namedtuple('SdPair',"i_s R_s db")):
+
     def __eq__(self, other):
         true_class = isinstance(other,self.__class__)
         true_solute = self.i_s == other.i_s and np.allclose(self.R_s,other.R_s)
         true_db = self.db == other.db
         return (true_class and true_solute and true_db)
-    
+
+    def __ne__(self,other):
+        return not self.__eq__(other)
+
     def __add__(self,other):
         if not isinstance(other,jump):
             raise TypeError("Can only add a jump to a state.")
         if not (self.db.i==other.i and self.db.c==other.c1 and np.allclose(self.db.R,other.R1) and np.allclose(self.db.o,other.or1)):
             raise ArithmeticError("Initial state of the jump must match current dumbbell state")
-        
+
         #check if mixed dumbbell jump
         if(self.i_s==self.db.i and np.allclose(self.R_s,self.db.R) and self.db.c==1):
-            return self.__class__(other.j,other.R2,dumbbell(other.j,other.or_2,other.R2,other.c2))
-        
-        return self.__class__(self.i_s,self.R_s,dumbbell(other.j,other.or_2,other.R2,other.c2))
-            
+            return self.__class__(other.j,other.R2,dumbbell(other.j,other.or2,other.R2,other.c2))
+
+        return self.__class__(self.i_s,self.R_s,dumbbell(other.j,other.or2,other.R2,other.c2))
+
         #return self.__class__(self.i_s,self.R_s,other.R2,self.dx+other.dx,self.c)
-            
-    
+
+
     def gop(self,crys,chem,g):#apply group operation
         R_s_new, (ch,i_s_new) = crys.g_pos(g,self.R_s,(chem,self.i_s))
-        R_d_new, (ch,i_d_new) = crys.g_pos(g,self.R_d,(chem,self.i_d))
-        or_new = np.dot(g.cartrot,self.o)
-        dx_new = crys.g_direc(g, self.dx)
-        return self.__class__(i_s_new,i_d_new,or_new,R_s_new,R_d_new,dx_new,self.c)
-        
+        dbnew = self.db.gop(crys,chem,g)
+        return self.__class__(i_s_new,R_s_new,dbnew)
+
 
 
 # Jump objects are rather simple, contain just initial and final orientations, location and pointers towards jumping/active atom (+1 for head of orientation vector, -1 for tail of orientation vector).
@@ -95,26 +95,34 @@ class pairs(namedtuple('pairs',"i_s R_s db")):
 
 
 class jump(namedtuple('jump','i j or1 or2 R1 R2 c1 c2')):
-    
+
     def __init__(self,i,j,or1,or2,R1,R2,c1,c2):#How to go about doing this in a better way?
         self.dx = self.R2-self.R1
-        
+
+    def __eq__(self,other):
+        true_object = self.__class__ == other.__class__
+        true_indices = self.i==other.i and self.c1==other.c1 and self.c2==other.c2
+        true_orientations = np.allclose(self.or1,other.or1) and np.allclose(self.or2,other.or2)
+        true_locations = np.allclose(self.R1,other.R1) and np.allclose(self.R2,other.R2)
+        return (true_object and true_indices and true_orientations and true_locations)
+
+    def __ne__(self,other):
+        return not self.__eq__(other)
+
     def __add__(self,other):
         if not isinstance(other,self.__class__):
             raise TypeError("Can only add two jumps.")
-            
-        if not (self.j==other.i and np.allclose(self.or2,other.or1),np.allclose(self.R2,other.R1),
-                self.c2==other.c1):
+
+        if not (self.j==other.i and np.allclose(self.or2,other.or1) and np.allclose(self.R2,other.R1)
+                and self.c2==other.c1):
             raise ArithmeticError("Starting point of second jump does not match end point of first")
-            
+
         return self.__class__(self.i,other.j,self.or1,other.or2,self.R1,other.R2,
                               self.c1,other.c2)
-    
-    def gop(self,crys,chem,g): #Find symmetry equivalent jumps - required when making composite jumps.
-        R1new,inew = crys.g_pos(g, R1, (chem, self.i))
-        R2new,jnew = crys.g_pos(g, R2, (chem, self.j))
-        or1_new = np.dot(g.cartrot,or1)
-        or2_new = np.dot(g.cartrot,or2)
-        return self.__class__(inew,jnew,or1_new,or2_new,R1new,R2new,self.c1,self.c2)
-        
 
+    def gop(self,crys,chem,g): #Find symmetry equivalent jumps - required when making composite jumps.
+        R1new,inew = crys.g_pos(g, self.R1, (chem, self.i))
+        R2new,jnew = crys.g_pos(g, self.R2, (chem, self.j))
+        or1_new = np.dot(g.cartrot,self.or1)
+        or2_new = np.dot(g.cartrot,self.or2)
+        return self.__class__(inew,jnew,or1_new,or2_new,R1new,R2new,self.c1,self.c2)
--- test_representations.py
+++ test_representations.py
@@ -8,7 +8,7 @@ import numpy as np
 import numpy.linalg as la
 import onsager.crystal as crystal
 import unittest
-import representations
+from representations import *
 from collections import namedtuple
 
 
@@ -25,14 +25,14 @@ class DB_Tests(unittest.TestCase):
     Tests related to a single dumbell diffusing in a lattice.
     Test case - tetragonal lattice.
     Format of a dumbbell object - "i o R (+-)1" -> basis site, orientation, unit cell location, jumping
-                                   atom indicator respectively. 
+                                   atom indicator respectively.
     """
-    def setup():
-        """
-        Setup a tetragonal crystal to run the tests.
-        """
-        self.crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
-    
+    # def setup(self):
+    #     """
+    #     Setup a tetragonal crystal to run the tests.
+    #     """
+    #     crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
+
     #test equality comparison
     def test_equality(self):
         or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))#orientation 1
@@ -42,20 +42,21 @@ class DB_Tests(unittest.TestCase):
         db3 = dumbbell(0,or_2,np.array([1.,0.,0.]),1)
         self.assertEqual(db3,db2) #test equality and inequality operators
         self.assertNotEqual(db1,db3)
-        
+
     #test addition with a jump object
     def test_jump(self):
         or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
-        db1 = dumbbell(0,or_1,np.array([1.,0.,0.]),1)
+        db1 = dumbbell(0,or_1,np.array([0.,0.,0.]),1)
         or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))
         j1 = jump(0,0,or_1,or_2,np.array([0.,0.,0.]),np.array([1.,0.,0.]),1,1)
         db_f = dumbbell(0,or_2,np.array([1.,0.,0.]),1)
         with self.assertRaises(ArithmeticError):
             db_f+j1
         self.assertEqual(db_f,db1+j1)
-        
+
     #Test Application of group operations
     def test_gop(self):
+        crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
         or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
         db1 = dumbbell(0,or_1,np.array([1.,0.,0.]),1)
         db_list=[]
@@ -79,30 +80,38 @@ class SdPair_Tests(unittest.TestCase):
     """
     Tests related to a single dumbell diffusing in a lattice.
     Test case - tetragonal lattice.
-    Format of a pair object - "i_s R_s db" -> solute location (state), dumbbell state 
+    Format of a pair object - "i_s R_s db" -> solute location (state), dumbbell state
     """
-    def setup():
-        """
-        Setup a tetragonal crystal to run the tests.
-        """
-        self.crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
-    
+    # def setup():
+    #     """
+    #     Setup a tetragonal crystal to run the tests.
+    #     """
+    #     self.crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
+
     or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))#orientation 1
     or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))#orientation 2
     or_x = np.array([1.,0.,0.])/la.norm(np.array([1.,0.,0.]))#orientation along x
-    
+
     db1 = dumbbell(0,or_1,np.array([0.,0.,0.]),1)#create a dumbbell state
     db1n = dumbbell(0,or_1,np.array([0.,0.,0.]),-1)#create a dumbbell state
     db2 = dumbbell(0,or_2,np.array([1.,0.,0.]),1)
-    
+
     #test equality comparison
     def test_equality(self):
+        or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))#orientation 1
+        or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))#orientation 2
+        or_x = np.array([1.,0.,0.])/la.norm(np.array([1.,0.,0.]))#orientation along x
+
+        db1 = dumbbell(0,or_1,np.array([0.,0.,0.]),1)#create a dumbbell state
+        db1n = dumbbell(0,or_1,np.array([0.,0.,0.]),-1)#create a dumbbell state
+        db2 = dumbbell(0,or_2,np.array([1.,0.,0.]),1)
+
         pair1 = SdPair(0,np.array([1.,0.,0.]),db1)
         pair2 = SdPair(0,np.array([1.,0.,0.]),db2)
         pair3 = SdPair(0,np.array([1.,0.,0.]),db2)
         self.assertEqual(pair3,pair2) #test equality and inequality
         self.assertNotEqual(pair1,pair3)
-        
+
     #test addition with a jump object
     def test_jump(self):
         """
@@ -112,55 +121,69 @@ class SdPair_Tests(unittest.TestCase):
             2. Put solute and dumbbell in different locations - execute dumbbell jumps.
         """
         #1a. - solute jumps, c=+1
+        or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
+        or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))
+
+        db1 = dumbbell(0,or_1,np.array([0.,0.,0.]),1)
+
         pair1 = SdPair(0,np.array([0.,0.,0.]),db1)
+
         j1 = jump(0,0,or_1,or_2,np.array([0.,0.,0.]),np.array([1.,0.,0.]),1,1)
-        db2 = (0,or_2,np.array([1.,0.,0.]),1)
+
+        db2 = dumbbell(0,or_2,np.array([1.,0.,0.]),1)
+
         pair2w = SdPair(0,np.array([0.,0.,0.]),db2)#solute doesn't move - wrong
         pair2r = SdPair(0,np.array([1.,0.,0.]),db2)#solute moves - right
-        
+
         with self.assertRaises(ArithmeticError):
             pair2r+j1
-        
+
         self.assertEqual(pair1+j1,pair2r)
-        sefl.assertNotEqual(pair1+j1,pair2w)
-        
-        #1b - solute stays fixed, c=-1
+        self.assertNotEqual(pair1+j1,pair2w)
+
+            #1b - solute stays fixed, c=-1
+        db1n = dumbbell(0,or_1,np.array([0.,0.,0.]),-1)
         pair1 = SdPair(0,np.array([0.,0.,0.]),db1n) #mixed dumbbell with solvent as active atom.
         j1 = jump(0,0,or_1,or_2,np.array([0.,0.,0.]),np.array([1.,0.,0.]),-1,1)
-        db2 = (0,or_2,np.array([1.,0.,0.]),1)
+        db2 = dumbbell(0,or_2,np.array([1.,0.,0.]),1)
         pair2r = SdPair(0,np.array([0.,0.,0.]),db2)#solute doesn't move - right
         pair2w = SdPair(0,np.array([1.,0.,0.]),db2)#solute moves - wrong
         self.assertEqual(pair1+j1,pair2r)
-        sefl.assertNotEqual(pair1+j1,pair2w)
-        
+        self.assertNotEqual(pair1+j1,pair2w)
+
         #2. isolated dumbbell jump - jumps onto solute site
         pair1 = SdPair(0,np.array([1.,0.,0.]),db1)
         j1 = jump(0,0,or_1,or_2,np.array([0.,0.,0.]),np.array([1.,0.,0.]),1,1)
-        db2 = (0,or_2,np.array([1.,0.,0.]),1)#dumbbell at the solute site
+        db2 = dumbbell(0,or_2,np.array([1.,0.,0.]),1)#dumbbell at the solute site
         pair2 = SdPair(0,np.array([1.,0.,0.]),db2)
         self.assertEqual(pair1+j1,pair2)
-        
-        
+
+
     #Test Application of group operations
     def test_gop(self):
+        crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
+        or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
+        db1 = dumbbell(0,or_1,np.array([0.,0.,0.]),1)
         pair1 = SdPair(0,np.array([1.,0.,0.]),db1)
-        db2 = dumbbell(0,or_x,np.array([1.,0.,0.]))
-        pair2 = sdPair(0,np.array([0.,0.,0.]),db2)
-        
+
+        or_x = np.array([1.,0.,0.])/la.norm(np.array([1.,0.,0.]))#orientation along x
+        db2 = dumbbell(0,or_x,np.array([0.,0.,1.]),1)
+        pair2 = SdPair(0,np.array([0.,0.,0.]),db2)
+
         pair_list1=[]
         pair_list2=[]
         for g in crys.G:
             pairn = pair1.gop(crys,0,g)
             if not any(pair==pairn for pair in pair_list1):
                 pair_list1.append(pairn)
-        
+
         for g in crys.G:
             pairn = pair2.gop(crys,0,g)
             if not any(pair==pairn for pair in pair_list2):
                 pair_list2.append(pairn)
         self.assertEqual(len(pair_list1),8)
         self.assertEqual(len(pair_list2),8) #4 fold symmetry about z, 2fold about x and y.
-        #should have 4 equivalent positions, and 2 states each for +- vectors.
+        #should have 2 equivalent positions, and 4 states each.
 
 
 # For jump objects, need to check the following two for now:
@@ -171,35 +194,41 @@ class SdPair_Tests(unittest.TestCase):
 
 
 class jump_Tests(unittest.TestCase):
-    
+
     def setup():
         """
         Setup a tetragonal crystal to run the tests.
         """
         self.crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
-    
+
     or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))#orientation 1
     or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))#orientation 2
     or_3 = np.array([1.,0.,1.])/la.norm(np.array([1.,0.,1.]))#orientation 3
-    
+
     def test_dx(self):
-        j = jump(0,0,or_1,or_2,np.array([0.,1.,0.]),np.array([0.,0.,0.]),np.array([0.,-1.,0.]),1,1)
-        self.assertTrue(np.allclose(j.dx,j.R_d-j.R_s)
+        or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
+        or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))
+        j = jump(0,0,or_1,or_2,np.array([0.,1.,0.]),np.array([0.,0.,0.]),1,1)
+        self.assertTrue(np.allclose(j.dx,j.R2-j.R1)
                        )
     def test_add(self):
+        or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
+        or_2 = np.array([0.,1.,1.])/la.norm(np.array([0.,1.,1.]))
+        or_3 = np.array([1.,0.,1.])/la.norm(np.array([1.,0.,1.]))
         j1 = jump(0,0,or_1,or_2,np.array([0.,1.,0.]),np.array([0.,0.,0.]),1,1)
         j2 = jump(0,0,or_2,or_3,np.array([0.,0.,0.]),np.array([0.,0.,1.]),1,1)
         j3 = jump(0,0,or_1,or_3,np.array([0.,1.,0.]),np.array([0.,0.,1.]),1,1)
         with self.assertRaises(ArithmeticError):
             j3+j2
         self.assertEqual(j3,j1+j2)
-    
+
     def test_gop(self):
-        j3 = jump(0,0,or_1,or_2,np.array([0.,1.,0.]),np.array([0.,0.,1.]),np.array([0.,-1.,1.]),1,1)
+        crys = crystal.Crystal(np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.5]]),[[np.zeros(3)]])
+        or_1 = np.array([0.,0.,1.])/la.norm(np.array([0.,0.,1.]))
+        j = jump(0,0,or_1,or_1,np.array([0.,1.,0.]),np.array([0.,0.,1.]),1,1)
         symm_jump_list=[]
         for g in crys.G:
-            j2 = pair1.gop(crys,0,g)
-            if not any(j==j2 for j in symm_jump_list):
-                pair_list.append(j2)
-        self.assertEqual(len(symm_jump_list),16)
-
+            j1 = j.gop(crys,0,g)
+            if not any(j1==j2 for j2 in symm_jump_list):
+                symm_jump_list.append(j1)
+        self.assertEqual(len(symm_jump_list),8)
