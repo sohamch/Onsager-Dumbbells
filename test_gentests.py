d5168463c559b4e1d4ec0c4a5e0d1931498cba58
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-06-21 13:32:05 -0500

    Added tests for pure (i,or) sets

---
+++ test_gentests.py
@@ -0,0 +1,16 @@
+import numpy as np
+import onsager.crystal as crystal
+from gensets import *
+from test_structs import *
+import unittest
+
+class test_sets(unittest.TestCase):
+
+    def test_genpurestates(self):
+        famp0 = [np.array([1.,1.,0.]),np.array([1.,0.,0.])]
+        famp12 = [np.array([1.,1.,1.]),np.array([1.,1.,0.])]
+        family = [famp0,famp12]
+        pairs_pure = genpuresets(tet2,0,family)
+        self.assertEqual(len(pairs_pure),len(tet2.sitelist(0)))
+        self.assertEqual(len(pairs_pure[0]),4)
+        #Check the (i,or) pairs for sites 1 and 2 manually.
