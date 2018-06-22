51333358963b21011be7e18a71ba916a78738217
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-06-22 15:48:45 -0500

    Incorporating changes.

--- states.py
+++ states.py
@@ -10,7 +10,7 @@ class dbStates(object):
     to symmetry grouped pairs.
                       2. Given an (i,or) pair, check which pair in the set a group operation maps it onto.
     """
-    def __init__(self,crys,chem,famlist):
+    def __init__(self,crys,chem,iorlist):
         if not isinstance(famlist,list):
             raise TypeError("Enter the families as a list of lists")
         for i in famlist:
@@ -21,53 +21,58 @@ class dbStates(object):
                     raise TypeError("Enter individual orientation families as numpy arrays")
         self.crys = crys
         self.chem = chem
-        self.famlist = famlist
-        self.iorlist = self.genpuresets()
+        self.iorlist = iorlist
+        self.symorlist = self.gensymset(crys,chem,iorlist)
+        #Store both iorlist and symorlist so that we can compare them later if needed.
+        self.threshold = crys.threshold
 
-    def gdumb(self,g,jmp):
+    def gdumb(self,g,db):
         """
-        Takes as an argument a jump that occurs in the given sublattice and
-        return the valid result of a group operation
+        Takes as an argument a dumbbell and return the result of a group operation on that dumbbell
+        param: g - group operation
+               db - dumbbell object to operate upon
+        returns - (db_new,p) - the dumbbell produced by the symmetry operation and the parity value depending on existence
+                  within symorlist.
+                  Example - 1. If the new orientation produced is [-100] but the one present in symorlist is [100], then returns
+                    ("db object with orientation [100]", -1)
+                            2. If the new orientation is [100] instead, returns ("db object with orientation [100]", 1)
         """
         def inlist(tup):
-            return any(tup[0]==x[0] and np.allclose(tup[1],x[1],atol=1e-8) for lis in self.iorlist for x in lis)
+            return any(tup[0]==x[0] and np.allclose(tup[1],x[1],atol=self.threshold) for lis in self.symorlist for x in lis)
 
-        if not isinstance(jmp.state1,dumbbell):
-            raise TypeError("This group operation is only valid for dumbbells, not pairs.")
-        jmp_new = jmp.gop(self.crys,self.chem,g)
-        parity=[]
-        for db in [jmp_new.state1,jmp_new.state2]:
-            i = db.i
-            o = db.o
-            if inlist((i,o)):
-                parity.append(1)
-            elif inlist((i,-o)):
-                parity.append(-1)
-        return (jmp_new,tuple(parity))
+        db_new = db.gop(self.crys,self.chem,g)
+        i = db_new.i
+        o = db_new.o
+        tup = None
+        if inlist((i,o)):
+            tup = tuple([db_new,1])
+        elif inlist((i,-o)):
+            db_new = dumbbell(db_new.i,-db_new.o,db_new.R)
+            tup = tuple([db_new,-1])
+        if tup == None:
+            #This will be used only during the testing phase, can remove it later when not needed.
+            #Ideally, if the production of symorlist is correct, then it should catch either of the
+            #above two cases.
+            raise RuntimeError("The group operation does not produce an (i,or) set in symorlist")
+        return tup
 
-    def genpuresets(self):
-        crys,chem,family = self.crys,self.chem,self.famlist
-        def inlist(tup,lis):
-            return any(tup[0]==x[0] and np.allclose(tup[1],x[1],atol=1e-8) for x in lis) #for x in l)
+    def gensymset(crys,chem,iorlist):
 
-        def negOrInList(o,lis):
-            z=np.zeros(3)
-            return any(np.allclose(o+tup[1],z,atol=1e-8) for tup in lis)
-        sitelist = crys.sitelist(chem)
-        #Get the Wyckoff sets
-        pairlist=[]
-        for i,wycksites in enumerate(sitelist):
-            orlist = family[i]
-            symmlist=[]
-            site=wycksites[0]
-            newlist=[]
-            for o in orlist:
-                for g in crys.G:
-                    R, (ch,i_new) = crys.g_pos(g,np.zeros(3),(chem,site))
-                    o_new = crys.g_direc(g,o)
-                    if not (inlist((i_new,o_new),newlist) or inlist((i_new,-o_new),newlist)):
-                            if negOrInList(o_new,newlist):
-                                o_new = -o_new
-                            newlist.append((i_new,o_new))
-            pairlist.append(newlist)
-        return pairlist
+        def inlist(tup):
+            return any(tup[0]==x[0] and np.allclose(tup[1],x[1],atol=1e-8) for x in iorlist)
+
+        #first make a set of the unique orientations supplied - each taken only once
+        #That way we won't need to do redundant group operations
+        z = np.zeros(3)
+        orset=[]
+        for ior in iorlist:
+            if not any(i==x[0] and (np.allclose(tup[1],x[1],atol=crys.threshold) or np.allclose(tup[1]+x[1],z,atol=crys.threshold)) for x in orset):
+                orset.append(tup[1])
+
+        #Now, start with the first pair in orset
+        ior = orset[0]
+        symlist=[]
+        newlist=[]
+        newlist.append(tup)
+        for g in crys.G:
+            ior_new = tuple([g.indexmap[chem][ior[0]],crys.g_direc(g,ior[1])])
