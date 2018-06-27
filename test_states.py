14ebdb2de5d84d85694e0ff6bd3b174ebb5fd6be
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-06-27 15:59:14 -0500

    ior list for mixed and pure tested

--- test_states.py
+++ test_states.py
@@ -13,7 +13,20 @@ class test_sets(unittest.TestCase):
         family = [famp0,famp12]
         self.crys = tet2
         self.pairs_pure = genpuresets(tet2,0,family)
+        self.pairs_mixed = genmixedsets(tet2,0,family)
 
     def test_dbStates(self):
         dbstates=dbStates(self.crys,0,self.pairs_pure)
         self.assertEqual(len(dbstates.symorlist),4)
+
+    def test_mStates(self):
+        dbstates=dbStates(self.crys,0,self.pairs_pure)
+        mstates = mStates(self.crys,0,self.pairs_mixed)
+        self.assertEqual(len(mstates.symorlist),4)
+        for i in range(4):
+            self.assertEqual(len(dbstates.symorlist[i])/len(mstates.symorlist[i]),0.5)
+
+        mstates = mStates(self.crys,0,self.pairs_pure) #negative orientations are missing
+        self.assertEqual(len(mstates.symorlist),4)
+        for i in range(4):
+            self.assertEqual(len(dbstates.symorlist[i])/len(mstates.symorlist[i]),0.5)
