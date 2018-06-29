12daac1b039a3f1166cd45a9f9c72ef335856a20
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-06-29 17:48:30 -0500

    some changes to random runs done in the office

--- randomtests_mac.py
+++ randomtests_mac.py
@@ -13,44 +13,7 @@ len(pairs_pure)
 thrange=1
 sdpairs = genPairSets(tet2,0,pairs_pure,thrange)
 
-# db=dumbbell(0,np.array([0., 1., 0.]), np.array([1, 1, 0]))
-# pair = SdPair(0,np.array([0, 0, 0]),db)
-# pairlist = []
-# pairlist.append(pair)
-# for g in tet2.G:
-#     newpair = pair.gop(tet2,0,g)
-#     if not any(pair1==newpair for pair1 in pairlist):
-#         pairlist.append(newpair)
-# pairlist
-#
-# dblist = []
-# dblist.append(db)
-# for g in tet.G:
-#     newdb = db.gop(tet,0,g)
-#     if not any(db1==newdb or db1==-newdb for db1 in dblist):
-#         dblist.append(newdb)
-#         print (crystal.GroupOp.optype(g.rot))
-# dblist
-#
-# or_1 = np.array([0.,1.,0.])
-# db1 = dumbbell(0,or_1,np.array([1.,0.,0.]))
-# pair1 = SdPair(0,np.array([1.,0.,0.]),db1)
-# pair_list=[]
-# pair_list.append(pair1)
-# for g in tet.G:
-#     pairn = pair1.gop(tet,0,g)
-#     if not any(pair==pairn for pair in pair_list):
-#         pair_list.append(pairn)
-# len(pair_list)
-# pair_list
-#
-# dblist = []
-# dblist.append(db1)
-# for g in tet.G:
-#     newdb = db.gop(tet,0,g)
-#     if not any(db==newdb for db in dblist):
-#         dblist.append(newdb)
-# dblist
+
 dblist=[]
 db=dumbbell(1, np.array([-1., -1.,  0.]), np.array([-1, -1,  0]))
 dblist.append(db)
