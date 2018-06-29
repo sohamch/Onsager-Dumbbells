cb72353a490276612cf55ea5d28f1ec9258de7d5
Soham Chattopadhyay <ch.soham@gmail.com>
2018-06-29 14:47:03 -0500

    cleaned up pair state generation

--- gensets.py
+++ gensets.py
@@ -29,7 +29,7 @@ def genpuresets(crys,chem,family):
                 R, (ch,i_new) = crys.g_pos(g,np.zeros(3),(chem,site))
                 o_new = crys.g_direc(g,o)
                 if not (inlist((i_new,o_new),pairlist) or inlist((i_new,-o_new),pairlist)):
-                        if negOrInList(o_new,newlist):
+                        if negOrInList(o_new,pairlist):
                             o_new = -o_new
                         pairlist.append((i_new,o_new))
     return pairlist
@@ -65,13 +65,22 @@ def genmixedsets(crys,chem,family):
 
 def genPairSets(crys,chem,iorlist,thrange):
 
+    def withinlist(db):
+        "returns a dumbbell that is within the iorlist by negating a vector if it has been reversed."
+        
+        if any(db.i==j and np.allclose(db.o,o1,atol=crys.threshold) for j,o1 in iorlist):
+            return db
+        if any(db.i==j and np.allclose(db.o+o1,0,atol=crys.threshold) for j,o1 in iorlist):
+            return -db
+
     def inlist(pair,lis):
-        return any(pair1==pair or -pair1==pair for pair1 in lis)
+        return any(pair==pair1 for pair1 in lis)
+
     #first create all the pairs within the thermodynamic range
     Rvects = [np.array([x,y,z]) for x in range(-thrange,thrange+1)
                       for y in range(-thrange,thrange+1)
                       for z in range(-thrange,thrange+1)]
-    print(Rvects)
+    # print(Rvects)
     pairlist=[]
     z=np.zeros(3).astype(int)
     for i_s in range(len(crys.basis[chem])):
@@ -85,10 +94,12 @@ def genPairSets(crys,chem,iorlist,thrange):
                     continue
                 # newlist=[]
                 pairlist.append(pair)
+                # print(pair)
                 for g in crys.G:
                     newpair = pair.gop(crys,chem,g)
-                    print(newpair)
+                    db = withinlist(newpair.db)
+                    newpair=SdPair(newpair.i_s,newpair.R_s,db)
                     if not inlist(newpair,pairlist):
                         pairlist.append(newpair)
-                print()
+                # print()
     return pairlist
