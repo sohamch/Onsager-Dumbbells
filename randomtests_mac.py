b97612a41f0b95ebc713cc23acba055466870c80
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-06-29 18:10:02 -0500

    added some random runs to put tests retrospectively

--- randomtests_mac.py
+++ randomtests_mac.py
@@ -23,3 +23,20 @@ for g in tet2.G:
         dblist.append(newdb)
         # print (crystal.GroupOp.optype(g.rot))
 dblist
+
+famp0 = [np.array([1.,0.,0.])]
+famp12 = [np.array([1.,1.,0.])]
+family = [famp0,famp12]
+pairs_pure = genpuresets(tet2,0,family)
+pairset = genPairSets(tet2,0,pairs_pure,1)
+pset = Pairstates(tet2,0,pairset)
+count=0
+for plis in pset.sympairlist:
+    for pair in plis:
+        if pair.db.i==0 and np.allclose(pair.db.R,np.array([1,1,0]),atol=1e-8):
+            count=1
+            lis=plis.copy()
+            break
+    if count == 1:
+        break
+    
