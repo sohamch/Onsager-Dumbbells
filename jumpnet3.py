be34ddf6376de1b5e56677211c97239d158df76b
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-07-17 18:19:40 -0500

    Added hash comparisons in place of object comparison.

--- jumpnet3.py
+++ jumpnet3.py
@@ -3,7 +3,7 @@ import onsager.crystal as crystal
 from representations import *
 from states import *
 from collision import *
-
+import time
 def purejumps(crys,chem,iorset,cutoff,solv_solv_cut,closestdistance):
     """
     Makes a jumpnetwork of pure dumbbells within a given distance to be used for omega_0
@@ -16,51 +16,87 @@ def purejumps(crys,chem,iorset,cutoff,solv_solv_cut,closestdistance):
         closestdistance - minimum allowable distance to check for collisions with other atoms. Can be a single
         number or a list (corresponding to each sublattice)
     """
+    def inset(j,s):
+        return hash(j) in s
+
+    def inlist(j,l):
+        return any(hash(j)==hash(j1) for j1 in l)
+
+    def dx_excess(db1,db2):
+        (i1,i2) = (db1.i,db2.i)
+        (R1,R2) = (db1.R,db2.R)
+        dR = crys.unit2cart(R2,crys.basis[chem][i2]) - crys.unit2cart(R1,crys.basis[chem][i1])
+        if np.dot(dR,dR) > cutoff**2:
+            return True
+
     nmax = [int(np.round(np.sqrt(cutoff**2/crys.metric[i,i]))) + 1 for i in range(3)]
     Rvects = [np.array([n0,n1,n2]) for n0 in range(-nmax[0],nmax[0]+1)
                                  for n1 in range(-nmax[1],nmax[1]+1)
                                  for n2 in range(-nmax[2],nmax[2]+1)]
     jumplist=[]
+    hashset=set([])
+    tcheck=[]
+    tlen = []
     dbstates = dbStates(crys,chem,iorset)
+    print("Entering main loop")
+    count=0
     for R in Rvects:
         for i in iorset:
             for f in iorset:
                 db1 = dumbbell(i[0],i[1],np.array([0,0,0]))
-                db1 = dumbbell(f[0],f[1],R)
+                db2 = dumbbell(f[0],f[1],R)
                 if db1==db2:#catch the diagonal case
+                    continue
+                if dx_excess(db1,db2):
                     continue
                 for c1 in[-1,1]:
                     rotcheck = i[0]==f[0] and np.allclose(R,0,atol=crys.threshold)
                     if rotcheck:
                         j = jump(db1,db2,c1,1)
-                        if not inset(j,jumplist) and not (collision_self(crys,chem,j,solv_solv_cut,solv_solv_cut) or collision_others(crys,chem,j,closestdistance)):
-                            jlist=[]
-                            jlist.append(j)
-                            for g in crys.G:
-                                # jnew = j.gop(crys,chem,g)
-                                db1new = dbstates.gdumb(g,db1)
-                                db2new = dbstates.gdumb(g,db2)
-                                jnew = jump(db1new[0],db2db2new[0],c1*db1new[1],1*db2new[1])
-                                if not inlist(jnew,jlist):
-                                    jlist.append(jnew)
-                            jumplist.append(jlist)
-                    for c2 in [-1,1]:
-                        if rotcheck:
+                        start = time.time()
+                        cond=inset(j,hashset)
+                        tcheck.append(time.time()-start)
+                        tlen.append(len(hashset))
+                        if cond: #no point doing anything else if the jump has already been considered
                             continue
-                        j = jump(db1,db2,c1,c2)
-                        if not inset(j,jumplist) and not (collision_self(crys,chem,j,solv_solv_cut,solv_solv_cut) or collision_others(crys,chem,j,closestdistance)):
+                        if not (collision_self(crys,chem,j,solv_solv_cut,solv_solv_cut) or collision_others(crys,chem,j,closestdistance)):
+                            #If the jump has not already been considered, check if it leads to collisions.
                             jlist=[]
-                            jlist.append(j)
                             for g in crys.G:
                                 # jnew = j.gop(crys,chem,g)
                                 db1new = dbstates.gdumb(g,db1)
                                 db2new = dbstates.gdumb(g,db2)
-                                jnew = jump(db1new[0],db2db2new[0],c1*db1new[1],c2*db2new[1])
-                                if not inlist(jnew,jlist):
+                                jnew = jump(db1new[0],db2new[0],c1*db1new[1],1*db2new[1])
+                                if not inset(jnew,hashset):
                                     jlist.append(jnew)
-                                    jlist.append(-jnew)
+                                    # jlist.append(-jnew)
+                                    hashset.add(hash(jnew))
+                                    # hashset.add(hash(-jnew))
                             jumplist.append(jlist)
-    return jumplist
+                            continue
+                    for c2 in [-1,1]:
+                        j = jump(db1,db2,c1,c2)
+                        start = time.time()
+                        cond=inset(j,hashset)
+                        tcheck.append(time.time()-start)
+                        tlen.append(len(hashset))
+                        if cond: #no point doing anything else if the jump has already been considered
+                            continue
+                        if not (collision_self(crys,chem,j,solv_solv_cut,solv_solv_cut) or collision_others(crys,chem,j,closestdistance)):
+                            #If the jump has not already been considered, check if it leads to collisions.
+                            jlist=[]
+                            for g in crys.G:
+                                # jnew = j.gop(crys,chem,g)
+                                db1new = dbstates.gdumb(g,db1)
+                                db2new = dbstates.gdumb(g,db2)
+                                jnew = jump(db1new[0],db2new[0],c1*db1new[1],c2*db2new[1])
+                                if not inset(jnew,hashset):
+                                    jlist.append(jnew)
+                                    # jlist.append(-jnew)
+                                    hashset.add(hash(jnew))
+                                    # hashset.add(hash(-jnew))
+                            jumplist.append(jlist)
+    return jumplist,tcheck,tlen
 
 def mixedjumps(crys,chem,iorset,cutoff,solt_solv_cut,closestdistance):
     """
