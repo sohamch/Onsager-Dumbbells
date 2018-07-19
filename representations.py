20b1fc7b1975938464ed6897c6776fc10982726b
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-07-19 17:04:32 -0500

    Needed to change addition of jumps. Verify with Dallas.

--- representations.py
+++ representations.py
@@ -74,6 +74,27 @@ class SdPair(namedtuple('SdPair',"i_s R_s db")):
         """
         return self.i_s==self.db.i and np.allclose(self.R_s,self.db.R)
 
+    def addjump(self,j,mixed=False):
+        if mixed==False and isinstance(j.state1,SdPair):
+            raise TypeError("Only dumbbell -> dumbbell transitions can be added to complexes, not pair -> pair")
+        elif mixed==True and not isinstance(j.state1,SdPair):
+            raise TypeError("Only pair -> pair transitions can be added to mixed dumbbells")
+
+        if mixed==False:
+            if not (self.db.i==j.state1.i and np.allclose(self.db.o,j.state1.o)):
+                raise ArithmeticError("Incompatible starting dumbbell configurations")
+            db2 = dumbbell(j.state2.i,j.state2.o,self.db.R+j.state2.R-j.state1.R)
+            return self.__class__(self.i_s,self.R_s,db2)
+
+        if mixed==True:
+            if not self.is_zero():
+                raise TypeError("You indicated that a complex is a mixed dumbbell.")
+            if not (self.db.i==j.state1.db.i and np.allclose(self.db.o,j.state1.db.o)):
+                raise ArithmeticError("Incompatible starting dumbbell configurations")
+            #see if solute atom must move
+            db2 = dumbbell(j.state2.db.i,j.state2.db.o,j.state2.db.R + self.db.R-j.state1.db.R)
+            soluteshift = j.state2.R_s - j.state1.R_s
+            return SdPair(j.state2.i_s,soluteshift+self.R_s,db2)
 # Jump obects are rather simple, contain just initial and final orientations
 # Also adding a jump to a dumbbell is now done here.
 # dumbell/pair obects are not aware of jump dbects.
@@ -92,7 +113,7 @@ class jump(namedtuple('jump','state1 state2 c1 c2')):
                 # if not self.c1==self.c2:
                 #     raise ArithmeticError("The same active atom must transition between state1 and state2 (must have same c values) for a mixed dumbbell")
                 if self.c1==1:
-                    if not (self.state2.i_s==self.state2.db.i and np.allclose(self.state2.R_s,self.state2.db.R)):
+                    if not (self.state2.i_s==self.state2.db.i and np.allclose(self.state2.R_s,self.state2.db.R) and self.c2==1):
                         raise ArithmeticError("Solute atom jumping from mixed dumbbell must lead to another mixed dumbbell")
                 if self.c1==-1:
                     if not (self.state2.i_s==self.state1.i_s and np.allclose(self.state1.R_s,self.state2.R_s)):
@@ -109,37 +130,42 @@ class jump(namedtuple('jump','state1 state2 c1 c2')):
 
     def __add__(self,other):
           #Do type checking of input operands and jump states
-        if not (isinstance(self.state1,other.__class__) and isinstance(self.state2,other.__class__)):
-            raise TypeError("Incompatible operand and jump states.")
-        if isinstance(other,dumbbell):
-          #check that the initial dumbbell state of the jump and the input dumbbell
-          #have the same configuration
-            if not (np.allclose(self.state1.o,other.o) and self.state1.i==other.i):
-                raise ArithmeticError("Operand dumbbell and initial dumbbell of jump must have same configuration (basis site coordinate and orientation")
-            return (dumbbell(self.state2.i,self.state2.o,other.R+self.state2.R))
-        if isinstance(other,SdPair):
-          #Check arithmetic compatibility of dumbbells
-            if not (np.allclose(self.state1.db.o,other.db.o) and self.state1.db.i==other.db.i):
-                raise ArithmeticError("Operand pair's dumbbell and initial pair's dumbell of jump must have same configuration (basis site coordinate and orientation")
-              #check that the pair is translated with the same pair configuration.
-            if not (self.state1.i_s == other.i_s and np.allclose(-self.state1.R_s+self.state1.db.R,-other.R_s+other.db.R)):
-                raise ArithmeticError("Operand pair and initial pair of jump must have same relative configurations")
-              #if not a mixed dumbbell, solute stays fixed
-            if not (other.i_s==other.db.i and np.allclose(other.R_s,other.db.R)):
-                dbnew = dumbbell(self.state2.db.i,self.state2.db.o,self.state2.db.R+other.R_s-self.state1.R_s)
-                return SdPair(other.i_s,other.R_s,dbnew)
-             #if a mixed dumbbell, check that the correct final state has been reached
-            if self.c1==1:
-                dbnew = dumbbell(self.state2.db.i,self.state2.db.o,self.state2.db.R+other.R_s-self.state1.R_s)
-                return SdPair(dbnew.i,dbnew.R,dbnew)
-            if self.c1==-1:
-                dbnew = dumbbell(self.state2.db.i,self.state2.db.o,self.state2.db.R+other.R_s-self.state1.R_s)
-                return SdPair(dbnew.i,dbnew.R,dbnew)
-        if isinstance(other,self.__class__):
-            #Add two jumps
-            if not(self.state2==other.state1 and self.c2==other.c1):
-                raise ArithmeticError("Initial state of second jump must be the same as the final state of the first jump")
-                return self.__class__(self.state1,other.state2,self.c1,other.c2)
+          if not isinstance(other,dumbbell):
+              raise TypeError("For now jumps can only be added to dumbbell objects.")
+          if not (self.state1.i==other.i and np.allclose(self.state1.o,other.o)):
+              raise ArithmeticError("Operand dumbbell and initial dumbbell of jump must have same configuration.")
+          return dumbbell(self.state2.i,self.state2.o,self.state2.R+other.R)
+        # if not (isinstance(self.state1,other.__class__) and isinstance(self.state2,other.__class__)):
+        #     raise TypeError("Incompatible operand and jump states.")
+        # if isinstance(other,dumbbell):
+        #   #check that the initial dumbbell state of the jump and the input dumbbell
+        #   #have the same configuration
+        #     if not (np.allclose(self.state1.o,other.o) and self.state1.i==other.i):
+        #         raise ArithmeticError("Operand dumbbell and initial dumbbell of jump must have same configuration (basis site coordinate and orientation")
+        #     return (dumbbell(self.state2.i,self.state2.o,other.R+self.state2.R))
+        # if isinstance(other,SdPair):
+        #   #Check arithmetic compatibility of dumbbells
+        #     if not (np.allclose(self.state1.db.o,other.db.o) and self.state1.db.i==other.db.i):
+        #         raise ArithmeticError("Operand pair's dumbbell and initial pair's dumbell of jump must have same configuration (basis site coordinate and orientation")
+        #       #check that the pair is translated with the same pair configuration.
+        #     if not (self.state1.i_s == other.i_s and np.allclose(-self.state1.R_s+self.state1.db.R,-other.R_s+other.db.R)):
+        #         raise ArithmeticError("Operand pair and initial pair of jump must have same relative configurations")
+        #       #if not a mixed dumbbell, solute stays fixed
+        #     if not (other.i_s==other.db.i and np.allclose(other.R_s,other.db.R)):
+        #         dbnew = dumbbell(self.state2.db.i,self.state2.db.o,self.state2.db.R+other.R_s-self.state1.R_s)
+        #         return SdPair(other.i_s,other.R_s,dbnew)
+        #      #if a mixed dumbbell, check that the correct final state has been reached
+        #     if self.c1==1:
+        #         dbnew = dumbbell(self.state2.db.i,self.state2.db.o,self.state2.db.R+other.R_s-self.state1.R_s)
+        #         return SdPair(dbnew.i,dbnew.R,dbnew)
+        #     if self.c1==-1:
+        #         dbnew = dumbbell(self.state2.db.i,self.state2.db.o,self.state2.db.R+other.R_s-self.state1.R_s)
+        #         return SdPair(dbnew.i,dbnew.R,dbnew)
+        # if isinstance(other,self.__class__):
+        #     #Add two jumps
+        #     if not(self.state2==other.state1 and self.c2==other.c1):
+        #         raise ArithmeticError("Initial state of second jump must be the same as the final state of the first jump")
+        #         return self.__class__(self.state1,other.state2,self.c1,other.c2)
 
     def __radd__(self,other):
         return self.__add__(other)
