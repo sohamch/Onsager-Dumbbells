eb1e0be3a0c8c165647bae7e7f50880bc5cfb003
Soham Chattopadhyay <ch.soham@gmail.com>
2018-10-01 01:13:39 -0500

    Hopefully fixed bugs in rate expansions. Needs extensive tests.

--- vector_stars.py
+++ vector_stars.py
@@ -76,7 +76,7 @@ class vectorStars(object):
                                 self.vecvec.append(veclist)
             self.Nvstars = len(vecpos)
 
-    def biasexpansion(self,jumpnetwork_omega1,jumptype,jumpnetwork_omega4,jumpnetwork_omega3):
+    def biasexpansion(self,jumpnetwork_omega1,jumptype,jumpnetwork_omega34):
         """
         Returns an expansion of the bias vector in terms of the displacements produced by jumps.
         Parameters:
@@ -95,11 +95,11 @@ class vectorStars(object):
         bias0expansion = np.zeros((self.Nvstars_pure,len(self.starset.jumpindices)))
         bias1expansion = np.zeros((self.Nvstars_pure,len(jumpnetwork_omega1)))
 
-        bias4expansion = np.zeros((self.Nvstars_pure,len(jumpnetwork_omega4)))
+        bias4expansion = np.zeros((self.Nvstars_pure,len(jumpnetwork_omega34)))
         #Expansion of pure dumbbell state bias vectors and comple state bias vectors
 
         bias2expansion = np.zeros((self.Nvstars-self.Nvstars_pure,len(jumpnetwork_omega2)))
-        bias3expansion = np.zeros((self.Nvstars-self.Nvstars_pure,len(jumpnetwork_omega3)))
+        bias3expansion = np.zeros((self.Nvstars-self.Nvstars_pure,len(jumpnetwork_omega34)))
         #Expansion of mixed dumbbell state bias vectors.
 
         for i, states, vectors in zip(itertools.count(),self.vecpos[:Nvstars_pure],self.vecvec[:Nvstars_pure]):
@@ -118,16 +118,18 @@ class vectorStars(object):
                         bias0expansion[i, jt] += geom_bias
             #Next, omega_4: complex -> mixed
             #The correction delta_bias = bias4 + bias1 - bias0
-            for k,jumplist in zip(itertools.count(), jumpnetwork_omega4):
+            for k,jumplist in zip(itertools.count(), jumpnetwork_omega34):
                 for j in jumplist:
                     IS=j.state1
+                    if IS.is_zero(): #check if initial state is mixed dumbbell -> then skip
+                        continue
                     dx = disp(j)
                 # for i, states, vectors in zip(itertools.count(),self.vecpos,self.vecvec):
                     if states[0]==IS:
                         geom_bias = np.dot(vectors[0], dx) #I haven't normalized with respect to no. of states.
                         bias4expansion[i, k] += geom_bias
 
-        #Now, construct the bias2expansion and bias4expansion
+        #Now, construct the bias2expansion and bias3expansion
         for i, states, vectors in zip(itertools.count(),self.vecpos[Nvstars_pure:],self.vecvec[Nvstars_pure:]):
             #First construct bias2expansion
             #omega_2 : mixed -> mixed
@@ -140,8 +142,10 @@ class vectorStars(object):
                         geom_bias = np.dot(vectors[0], dx) #I haven't normalized with respect to no. of states.
                         bias2expansion[i, k] += geom_bias
             #Next, omega_3: mixed -> complex
-            for k,jumplist in zip(itertools.count(), jumpnetwork_omega3):
+            for k,jumplist in zip(itertools.count(), jumpnetwork_omega34):
                 for j in jumplist:
+                    if not IS.is_zero(): #check if initial state is a complex -> then skip
+                        continue
                     IS=j.state1
                     dx = disp(j)
                 # for i, states, vectors in zip(itertools.count(),self.vecpos,self.vecvec):
@@ -150,4 +154,52 @@ class vectorStars(object):
                         bias3expansion[i, k] += geom_bias
         return bias0expansion,bias1expansion,bias2expansion,bias3expansion,bias4expansion
 
-    def rateexpansion(self,jumpnetwork_omega1,jumptype,jumpnetwork_omega4,jumpnetwork_omega3)
+    def rateexpansion(self,jumpnetwork_omega1,jumptype,jumpnetwork_omega34):
+        rate0expansion = np.zeros((self.Nvstars_pure, self.Nvstars_pure, len(self.starset.jumpindices)))
+        rate1expansion = np.zeros((self.Nvstars_pure, self.Nvstars_pure, len(jumpnetwork)))
+
+        #First, we do the rate1 and rate0 expansions
+        for k,jumplist,jt in zip(itertools.count(), jumpnetwork_omega1, jumptype):
+            for jmp in jumplist:
+                for i in range(self.Nvstars_pure):
+                    for chi_i,vi in zip(self.vecpos[i],self.vecvec[i]):
+                        if chi_i==jmp.state1:
+                            for j in range(self.Nvstars_pure):
+                                for chi_j,vj in zip(self.vecpos[j],self.vecvec[j]):
+                                    if chi_j==jmp.state2:
+                                        rate1expansion[i,j,k] += np.dot(vi,vj)
+                                        rate0expansion[i,j,jt] += np.dot(vi,vj)
+        #Next, let's do the rate4expansion -> complex to mixed jumps
+        rate4expansion = np.zeros((self.Nvstars_pure,self.Nvstars-self.Nvstars_pure,len(jumpnetwork_omega34)))
+        #The initial states are complexes, the final states are mixed and there are as many symmetric jumps as in jumpnetwork_omega34
+        rate3expansion = np.zeros((self.Nvstars-self.Nvstars_pure,self.Nvstars_pure,len(jumpnetwork_omega34)))
+        #The initial states are mixed, the final states are complex and there are as many symmetric jumps as in jumpnetwork_omega34
+        for k,jumplist in zip(itertools.count(), jumpnetwork_omega34):
+            for jmp in jumplist:
+                if jmp.state1.is_zero(): #the initial state must be a complex
+                                         #the negative of this jump in a omega_3 jump anyway
+                    continue
+                for i in range(self.Nvstars_pure):
+                    for chi_i,vi in zip(self.vecpos[i],self.vecvec[i]):
+                        #Go through the initial pure states
+                        if chi_i==jmp.state1:
+                            for j in range(self.Nvstars_pure,self.Nvstars):
+                                for chi_j,vj in zip(self.vecpos[j],self.vecvec[j]):
+                                    #Go through the final complex states
+                                    if chi_j==jmp.state2:
+                                        rate4expansion[i,j,k] += np.dot(vi,vj)
+                                        rate3expansion[j,i,k] += np.dot(vj,vi)
+                                        #The type of jump remains the same because they have the same transition state
+
+        #Next, we do the rate2expansion for mixed->mixed jumps
+        rate2expansion = np.zeros((self.Nvstars-self.Nvstars_pure,self.Nvstars-self.Nvstars_pure, len(jumpnetwork_omega2)))
+        for k,jumplist in zip(itertools.count(), jumpnetwork_omega2):
+            for jmp in jumplist:
+                for i in range(self.Nvstars_pure,self.Nvstars):
+                    for chi_i,vi in zip(self.vecpos[i],self.vecvec[i]):
+                        if chi_i==jmp.state1:
+                            for j in range(self.Nvstars_pure,self.Nvstars):
+                                for chi_j,vj in zip(self.vecpos[j],self.vecvec[j]):
+                                    if chi_j.i_s==jmp.state2.i_s and np.allclose(chi_j.R_s,jmp.state2.R_s) and chi_j.db.i==jmp.state2.db.i and np.allclose(chi_j.db.o,jmp.state2.db.o):
+                                        rate2expansion[i,j,k] += np.dot(vi,vj)
+        return rate0expansion,rate1expansion,rate4expansion,rate3expansion,rate2expansion
