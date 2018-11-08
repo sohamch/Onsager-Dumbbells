906734b0b603c49cd3e26219c8af5e105d573bd5
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-11-08 15:53:24 -0600

    Changed generation of ior pairs. Tests updated. gensets internalized.

--- states.py
+++ states.py
@@ -2,6 +2,7 @@ import numpy as np
 import onsager.crystal as crystal
 from representations import *
 from shell import *
+from gensets import *
 
 class dbStates(object):
     """
@@ -11,14 +12,14 @@ class dbStates(object):
     to symmetry grouped pairs.
                       2. Given an (i,or) pair, check which pair in the set a group operation maps it onto.
     """
-    def __init__(self,crys,chem,iorlist):
-        if not isinstance(iorlist,list):
+    def __init__(self,crys,chem,family):
+        if not isinstance(family,list):
             raise TypeError("Enter the families as a list of lists")
 
         self.crys = crys
         self.chem = chem
-        self.iorlist = iorlist
-        self.symorlist = self.__class__.gensymset(crys,chem,iorlist)
+        self.iorlist = self.genpuresets(crys,chem,family)
+        self.symorlist = self.gensymset(crys,chem,self.iorlist)
         #Store both iorlist and symorlist so that we can compare them later if needed.
         self.threshold = crys.threshold
 
@@ -53,6 +54,41 @@ class dbStates(object):
             raise RuntimeError("The group operation does not produce an (i,or) set in symorlist")
         return tup
 
+    @staticmethod
+    def genpuresets(crys,chem,family):
+        if not isinstance(family,list):
+            raise TypeError("Enter the families as a list of lists")
+        for i in family:
+            if not isinstance(i,list):
+                raise TypeError("Enter the families for each site as a list of np arrays")
+            for j in i:
+                if not isinstance(j,np.ndarray):
+                    raise TypeError("Enter individual orientation families as numpy arrays")
+
+        def inlist(tup,lis):
+            return any(tup[0]==x[0] and np.allclose(tup[1],x[1],atol=1e-8) for x in lis)
+
+        def negOrInList(o,lis):
+            return any(np.allclose(o+tup[1],0,atol=1e-8) for tup in lis)
+
+        sitelist = crys.sitelist(chem)
+        #Get the Wyckoff sets
+        pairlist=[]
+        for i,wycksites in enumerate(sitelist):
+            orlist = family[i]
+            site=wycksites[0]
+            newlist=[]
+            for o in orlist:
+                for g in crys.G:
+                    R, (ch,i_new) = crys.g_pos(g,np.zeros(3),(chem,site))
+                    o_new = crys.g_direc(g,o)
+                    if not (inlist((i_new,o_new),pairlist) or inlist((i_new,-o_new),pairlist)):
+                            if negOrInList(o_new,pairlist):
+                                o_new = -o_new
+                            pairlist.append((i_new,o_new))
+        return pairlist
+
+    @staticmethod
     def gensymset(crys,chem,iorlist):
         """
         Takes in a flat list of (i,or) pairs and groups them according to symmetry
@@ -111,14 +147,14 @@ class mStates(object):
     to symmetry grouped pairs.
                       2. Given an (i,or) pair, check which pair in the set a group operation maps it onto.
     """
-    def __init__(self,crys,chem,iorlist):
-        if not isinstance(iorlist,list):
+    def __init__(self,crys,chem,family):
+        if not isinstance(family,list):
             raise TypeError("Enter the families as a list of lists")
 
         self.crys = crys
         self.chem = chem
-        self.iorlist = iorlist
-        self.symorlist = self.__class__.gensymset(crys,chem,iorlist)
+        self.iorlist = self.genmixedsets(crys,chem,family)
+        self.symorlist = self.gensymset(crys,chem,self.iorlist)
         #Store both iorlist and symorlist so that we can compare them later if needed.
         self.threshold = crys.threshold
 
@@ -147,6 +183,37 @@ class mStates(object):
             mdb_new=None
         return mdb_new
 
+    @staticmethod
+    def genmixedsets(crys,chem,family):
+
+        if not isinstance(family,list):
+            raise TypeError("Enter the families as a list of lists")
+        for i in family:
+            if not isinstance(i,list):
+                raise TypeError("Enter the families for each site as a list of numpy arrays")
+            for j in i:
+                if not isinstance(j,np.ndarray):
+                    raise TypeError("Enter individual orientation families as numpy arrays")
+
+        def inlist(tup,lis):
+            return any(tup[0]==x[0] and np.allclose(tup[1],x[1],atol=1e-8) for x in lis)
+
+        sitelist = crys.sitelist(chem)
+        #Get the Wyckoff sets
+        pairlist=[]
+        for i,wycksites in enumerate(sitelist):
+            orlist = family[i]
+            site = wycksites[0]
+            newlist=[]
+            for o in orlist:
+                for g in crys.G:
+                    R, (ch,i_new) = crys.g_pos(g,np.zeros(3),(chem,site))
+                    o_new = crys.g_direc(g,o)
+                    if not inlist((i_new,o_new),pairlist):
+                        pairlist.append((i_new,o_new))
+        return pairlist
+
+    @staticmethod
     def gensymset(crys,chem,iorlist):
         """
         Takes in a flat list of (i,or) pairs and groups them according to symmetry
--- test_states.py
+++ test_states.py
@@ -10,14 +10,15 @@ class test_sets(unittest.TestCase):
     def setUp(self):
         famp0 = [np.array([1.,1.,0.]),np.array([1.,0.,0.])]
         famp12 = [np.array([1.,1.,1.]),np.array([1.,1.,0.])]
-        family = [famp0,famp12]
+        self.family = [famp0,famp12]
         self.crys = tet2
-        self.pairs_pure = genpuresets(tet2,0,family)
-        self.pairs_mixed = genmixedsets(tet2,0,family)
+        # self.pairs_pure = genpuresets(tet2,0,family)
+        # self.pairs_mixed = genmixedsets(tet2,0,family)
+        #generating pairs from families is now taken care of within states
 
     def test_dbStates(self):
         #check that symmetry analysis is correct
-        dbstates=dbStates(self.crys,0,self.pairs_pure)
+        dbstates=dbStates(self.crys,0,self.family)
         self.assertEqual(len(dbstates.symorlist),4)
         #check that every (i,or) set is accounted for
         sm=0
@@ -42,8 +43,8 @@ class test_sets(unittest.TestCase):
         self.assertEqual(count,1)
 
     def test_mStates(self):
-        dbstates=dbStates(self.crys,0,self.pairs_pure)
-        mstates1 = mStates(self.crys,0,self.pairs_mixed)
+        dbstates=dbStates(self.crys,0,self.family)
+        mstates1 = mStates(self.crys,0,self.family)
 
         #check that symmetry analysis is correct
         self.assertEqual(len(mstates1.symorlist),4)
@@ -57,68 +58,19 @@ class test_sets(unittest.TestCase):
         for i in mstates1.symorlist:
             sm += len(i)
         self.assertEqual(sm,len(mstates1.iorlist))
-
+        #No longer necessary - requirement internally fulfilled
         #check without supplying negative orientations
-        mstates = mStates(self.crys,0,self.pairs_pure)
-        self.assertEqual(len(mstates.symorlist),4)
-        for i in range(4):
-            self.assertEqual(len(dbstates.symorlist[i])/len(mstates.symorlist[i]),0.5)
-
-        #check validity of group operations
-        Glist = list(self.crys.G)
-        x = np.random.randint(0,len(Glist))
-        g = Glist[x] #select random groupop
-        db=dumbbell(1, np.array([1.,1.,0.]), np.array([1,1,0]))
-        mdb = SdPair(db.i,db.R,db)
-        newmdb = mstates.gdumb(g,mdb)
-        self.assertTrue(newmdb.i_s==newmdb.db.i and np.allclose(newmdb.R_s,newmdb.db.R,atol=self.crys.threshold))
-        self.assertTrue(any(newmdb.db.i==tup[0] and np.allclose(newmdb.db.o,tup[1],atol=self.crys.threshold) for lis in mstates.symorlist for tup in lis))
-
-    def test_pairstates(self):
-        famp0 = [np.array([1.,1.,0.])]
-        famp12 = [np.array([1.,1.,0.])]
-        family = [famp0,famp12]
-        iorlist = genpuresets(tet2,0,family)
-        pset = Pairstates(tet2,0,iorlist,(4,1))
-        print("Pairstates object done")
-        count=0
-
-        #first extract the test set
-        count=0
-        for plis in pset.sympairlist:
-            for pair in plis:
-                if pair.db.i==0 and pair.i_s == 0 and np.allclose(abs(pair.db.R),np.array([1,0,0]),atol=1e-8):
-                    lis=plis.copy()
-                    count=1
-                    break
-            if count==1:
-                break
-        #check that symmetry grouping is correct
-        self.assertEqual(len(lis),8)
-
-        #test group operations
-        Glist = list(self.crys.G)
-        db = dumbbell(0,np.array([1.,1.,0.]),np.array([1,0,0]))
-        pair = SdPair(0,np.array([0,0,0]),db)
-        for t in range(5):
-            x = np.random.randint(0,len(Glist))
-            g = Glist[x] #select random groupop
-            pairnewtest = pair.gop(self.crys,0,g)
-            pairnew,p = pset.gpair(g,pair)
-            count=0
-            #check that proper parity is returned
-            if pairnewtest==pairnew:
-                self.assertEqual(p,1)
-                count=1
-            elif pairnewtest==-pairnew:
-                self.assertEqual(p,-1)
-                count=1
-            self.assertEqual(count,1)
-            #check that pairnew exists in the states list
-            check = False
-            for lis in pset.sympairlist:
-                for p in lis:
-                    if (p==pairnew):
-                        check = True
-                        break
-            self.assertTrue(check)
+        # mstates = mStates(self.crys,0,self.family)
+        # self.assertEqual(len(mstates.symorlist),4)
+        # for i in range(4):
+        #     self.assertEqual(len(dbstates.symorlist[i])/len(mstates.symorlist[i]),0.5)
+        #
+        # #check validity of group operations
+        # Glist = list(self.crys.G)
+        # x = np.random.randint(0,len(Glist))
+        # g = Glist[x] #select random groupop
+        # db=dumbbell(1, np.array([1.,1.,0.]), np.array([1,1,0]))
+        # mdb = SdPair(db.i,db.R,db)
+        # newmdb = mstates.gdumb(g,mdb)
+        # self.assertTrue(newmdb.i_s==newmdb.db.i and np.allclose(newmdb.R_s,newmdb.db.R,atol=self.crys.threshold))
+        # self.assertTrue(any(newmdb.db.i==tup[0] and np.allclose(newmdb.db.o,tup[1],atol=self.crys.threshold) for lis in mstates.symorlist for tup in lis))
