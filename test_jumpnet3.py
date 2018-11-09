041c1304e4bd3e75709fc960a96d324946d92ef4
sohamch <sohamc2@Trinkle-Groups-iMac.local>
2018-11-08 20:39:58 -0600

    Jnets now created with pdbobj and mdbobj master objects.

--- test_jumpnet3.py
+++ test_jumpnet3.py
@@ -3,17 +3,18 @@ from representations import *
 from jumpnet3 import *
 from test_structs import *
 from states import *
-from gensets import *
+# from gensets import *
 import unittest
 
 class test_jumpnetwork(unittest.TestCase):
     def setUp(self):
          famp0 = [np.array([1.,0.,0.])/np.linalg.norm(np.array([1.,0.,0.]))*0.126]
-         family = [famp0]
-         self.pairs_pure = genpuresets(cube,0,family)
+         self.family = [famp0]
+         # self.pairs_pure = genpuresets(cube,0,family)
 
     def test_purejumps(self):
-        jset = purejumps(cube,0,self.pairs_pure,0.3,0.01,0.01)[0]
+        pdbcontainer = dbStates(cube,0,self.family)
+        jset,jind = purejumps(pdbcontainer,0.3,0.01,0.01)
         test_dbi = dumbbell(0, np.array([0.126,0.,0.]),np.array([0,0,0]))
         test_dbf = dumbbell(0, np.array([0.126,0.,0.]),np.array([0,1,0]))
         count=0
@@ -27,9 +28,25 @@ class test_jumpnetwork(unittest.TestCase):
         self.assertEqual(count,1) #see that this jump has been taken only once into account
         self.assertEqual(len(jtest),24)
 
+        #test_indices
+        #First check if they have the same number of lists and elements
+        self.assertEqual(len(jind),len(jset))
+        #now check if all the elements are correctly correspondent
+        for lindex in range(len(jind)):
+            self.assertEqual(len(jind[lindex]),len(jset[lindex]))
+            for jindex in range(len(jind[lindex])):
+                (i1,o1) = pdbcontainer.iorlist[jind[lindex][jindex][0]]
+                (i2,o2) = pdbcontainer.iorlist[jind[lindex][jindex][1]]
+                self.assertEqual(jset[lindex][jindex].state1.i,i1)
+                self.assertEqual(jset[lindex][jindex].state2.i,i2)
+                self.assertTrue(np.allclose(jset[lindex][jindex].state1.o,o1))
+                self.assertTrue(np.allclose(jset[lindex][jindex].state2.o,o2))
+
+
     def test_mixedjumps(self):
+        mdbcontainer = mStates(cube,0,self.family)
         #check for the correct number of states
-        jset = mixedjumps(cube,0,self.pairs_pure,0.3,0.01,0.01)
+        jset,jind = mixedjumps(mdbcontainer,0.3,0.01,0.01)
         test_dbi = dumbbell(0, np.array([0.126,0.,0.]),np.array([0,0,0]))
         test_dbf = dumbbell(0, np.array([0.126,0.,0.]),np.array([0,1,0]))
         count=0
@@ -56,3 +73,17 @@ class test_jumpnetwork(unittest.TestCase):
             if count==1:
                 break
         self.assertEqual(count,0)
+
+        #test_indices
+        #First check if they have the same number of lists and elements
+        self.assertEqual(len(jind),len(jset))
+        #now check if all the elements are correctly correspondent
+        for lindex in range(len(jind)):
+            self.assertEqual(len(jind[lindex]),len(jset[lindex]))
+            for jindex in range(len(jind[lindex])):
+                (i1,o1) = mdbcontainer.iorlist[jind[lindex][jindex][0]]
+                (i2,o2) = mdbcontainer.iorlist[jind[lindex][jindex][1]]
+                self.assertEqual(jset[lindex][jindex].state1.db.i,i1)
+                self.assertEqual(jset[lindex][jindex].state2.db.i,i2)
+                self.assertTrue(np.allclose(jset[lindex][jindex].state1.db.o,o1))
+                self.assertTrue(np.allclose(jset[lindex][jindex].state2.db.o,o2))
