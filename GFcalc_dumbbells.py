daf67482acc1046cdca2d4f734cf2af45e923c71
Soham Chattopadhyay <ch.soham@gmail.com>
2018-11-10 23:48:07 -0600

    invmap and jumppairs solved.

--- GFcalc_dumbbells.py
+++ GFcalc_dumbbells.py
@@ -9,10 +9,10 @@ class GFCrystalcalc(object):
     """
     Class calculator for the Green function, designed to work with the Crystal class.
 
-    Many aspects similar to vacany GF calculator, indexing needs to change.
+    Highly similar to vacany GF calculator, indexing of jumps is in (i,or) list rather than basis set (i).
     """
 
-    def __init__(self, crys, chem, iorlist, symorlist, jumpnetwork, Nmax=4, kptwt = None):
+    def __init__(self, container, jumpnetwork, Nmax=4, kptwt = None):
         """
         Initializes our calculator with the appropriate topology / connectivity. Doesn't
         require, at this point, the site probabilities or transition rates to be known.
@@ -23,23 +23,23 @@ class GFCrystalcalc(object):
         :param symorlist: (basis_index,orientation) pairs grouped into symmetrically unique lists
         :param jumpnetwork: list of unique transitions as lists of ((i,j), dx, c1, c2)
                             "Note here i and j are indices into iorlist"
+                            "Needs to be ensured that the jumpnetwork belongs to the container"
         :param Nmax: maximum range as estimator for kpt mesh generation
         :param kptwt: (optional) tuple of (kpts, wts) to short-circuit kpt mesh generation
         """
         # this is really just used by loadHDF5() to circumvent __init__
-        if all(x is None for x in (crys, chem, iorlist, symorlist, jumpnetwork)): return
-        self.crys = crys
-        self.chem = chem
-        self.iorlist = iorlist
-        self.symorlist = symorlist.copy()
-        self.N = sum(len(w) for w in symorlist)#N - no. of dumbbell states
+        if all(x is None for x in (container, jumpnetwork)): return
+        if any (len(tup)!=5 for l in jumpnetwork for tup in l):
+            raise TypeError("Enter the jumpnetwork of the form (i,j,dx,c1,c2)")
+        self.crys = container.crys
+        self.chem = container.chem
+        self.iorlist = container.iorlist
+        self.symorlist = container.symorlist.copy()
+        self.N = len(iorlist)#N - no. of dumbbell states
         self.Ndiff = self.networkcount(jumpnetwork, self.N)
-        # self.invmap = np.zeros(self.N, dtype=int)
-        # for ind, w in enumerate(symorlist):
-        #     for i in range(len(w)):
-        #         self.invmap[i] = ind
-        # TODO: Figure out the inverse mapping into the flat list from the symmetric list
-        # TODO: Cannot use tuples with np arrays as keys as they are unhashable
+        #Create invmap - which symmety-grouped (i,or) pair list in symorlist
+        # does a given (i,or) pair in iorlist belong to
+        self.invmap = container.invmap #internalized in the container definition itself
         self.NG = len(self.crys.G)  # number of group operations
         self.grouparray, self.indexpair = self.BreakdownGroups()
         # note: currently, we don't store jumpnetwork. If we want to rewrite the class
@@ -47,13 +47,13 @@ class GFCrystalcalc(object):
         # a copy for regeneration
         # self.jumpnetwork = jumpnetwork
         # generate a kptmesh: now we try to make the mesh more "uniform" ??
-        bmagn = np.array([np.sqrt(np.dot(crys.reciplatt[:, i], crys.reciplatt[:, i]))
-                          for i in range(self.crys.dim)])
-        bmagn /= np.power(np.product(bmagn), 1 / self.crys.dim)
+        bmagn = np.array([np.sqrt(np.dot(self.crys.reciplatt[:, i], self.crys.reciplatt[:, i]))
+                          for i in range(3)])
+        bmagn /= np.power(np.product(bmagn), 1 / 3)
         # make sure we have even meshes - same as vacancies
         # Don't need to change the k-mesh generation.
         self.kptgrid = np.array([2 * np.int(np.ceil(2 * Nmax * b)) for b in bmagn], dtype=int) \
-            if kptwt is None else np.zeros(self.crys.dim, dtype=int)
+            if kptwt is None else np.zeros(3, dtype=int)
         self.kpts, self.wts = crys.reducekptmesh(crys.fullkptmesh(self.kptgrid)) \
             if kptwt is None else deepcopy(kptwt)
         self.Nkpt = self.kpts.shape[0]
@@ -61,10 +61,11 @@ class GFCrystalcalc(object):
         # also includes the multiplicity for the onsite terms (site expansion)
         self.FTjumps, self.SEjumps = self.FourierTransformJumps(jumpnetwork, self.N, self.kpts)
         # generate the Taylor expansion coefficients for each jump
+        #generate the jumppairs
+        self.jumppairs = tuple((self.invmap[jumplist[0][0]], self.invmap[jumplist[0][1]])
+                               for jumplist in jumpnetwork)
         self.Taylorjumps = self.TaylorExpandJumps(jumpnetwork, self.N)
-        # tuple of the Wyckoff site indices for each jump (needed to make symmrate)
-        # self.jumppairs = tuple((self.invmap[jumplist[0][0][0]], self.invmap[jumplist[0][0][1]])
-        #                        for jumplist in jumpnetwork)
+
         # TODO:jumppairs are used in SymmRates function - need to figure out inverse mapping
         self.D, self.eta = 0, 0  # we don't yet know the diffusivity
 
@@ -103,7 +104,7 @@ class GFCrystalcalc(object):
         :return T3Djumps: list of Taylor3D expansions of the jump network
         """
         #Soham - any change required? -> see the Fourier equation in my case
-        Taylor = T3D if self.crys.dim == 3 else T2D
+        Taylor = T3D
         Taylor()  # need to do just to initialize the class; if already initialized, won't do anything
         # Taylor expansion coefficients for exp(1j*x) = (1j)^n/n!
         pre = np.array([(1j) ** n / factorial(n, True) for n in range(Taylor.Lmax + 1)])
@@ -116,19 +117,20 @@ class GFCrystalcalc(object):
                 pexp = Taylor.powexp(dx, normalize=False) #make the powerexpansion for the components of dx
                 for n in range(Taylor.Lmax + 1):
                     (c[n][2])[:, i, j] += pre[n] * (Taylor.powercoeff[n] * pexp)[:Taylor.powlrange[n]]
-                    #look at the symobilc python notebook for each of these
+                    #look at the symoblic python notebook for each of these
             Taylorjumps.append(Taylor(c))
         return Taylorjumps
 
         def BreakdownGroups(self):
         """
+        Same as that for the case of vacancies.
         Takes in a crystal, and a chemistry, and constructs the indexing breakdown for each
         (i,j) pair.
         :return grouparray: array[NG][3][3] of the NG group operations
         :return indexpair: array[N][N][NG][2] of the index pair for each group operation
         """
         #Soham - no changes required to this
-        grouparray = np.zeros((self.NG, self.crys.dim, self.crys.dim))
+        grouparray = np.zeros((self.NG, 3, 3))
         indexpair = np.zeros((self.N, self.N, self.NG, 2), dtype=int)
         for ng, g in enumerate(self.crys.G):
             grouparray[ng, :, :] = g.cartrot[:, :]
