b54f78d2141b5f0dc5b995ca85009a11471fd5f4
Soham Chattopadhyay <ch.soham@gmail.com>
2018-11-24 22:20:07 -0600

    Building bare dumbbell onsagercalc.

---
+++ Onsager_calc_db.py
@@ -0,0 +1,68 @@
+import numpy as np
+from states import *
+from representations import *
+import GFcalc
+from functools import reduce
+import scipy.linalg as la
+
+class BareDumbbell(object):
+    """
+    class to compute Green's function for a bare interstitial dumbbell
+    diffusing through as crystal.
+    """
+    def __init__(self,container,jumpnetwork):
+        """
+        param: container - container object for dumbbell states
+        param: jumpnetwork - jumpnetwork (either omega_0 or omega_2)
+        """
+        self.container = container
+        self.jumpnetwork = jumpnetwork
+        self.VB,self.VV = self.FullVectorBasis()
+        self.NV = len(self.VB)
+
+        self.omega_invertible = True
+        if self.NV > 0:
+            self.omega_invertible = any(np.allclose(g.cartrot,-np.eye(3)) for g  in self.container.crys.G)
+        #What is this for though?
+        if self.omega_invertible:
+            self.bias_solver = lambda omega,b : -la.solve(-omega,b,sym_pos=True)
+        else:
+            # pseudoinverse required:
+            self.bias_solver = lambda omega, b: np.dot(pinv2(omega), b)
+
+        self.sitegroupops = self.generateStateGroupOps()
+        self.jumpgroupops = self.generateJumpGroupOps()
+
+    def generateStateGroupOps(self):
+        """
+        Returns a list of lists of groupOps that map the first element of each list in symorlist
+        to the corresponding elements in the same list.
+        """
+        glist=[]
+        for l in self.container.symorlist:
+            stind = None
+            tup0 = l[0]
+            # TODO: The part below needs to be done with dicts. How to make np array hashable?
+            for ind,tup in enumerate(self.container.iorlist):
+                if tup[0]==tup0[0] and np.allclose(tup[1],tup0[1]):
+                    stind=ind
+                    break
+            if stind==None:
+                raise RuntimeError("state not found in iorlist")
+            lis=[]
+            for ind,tup in enumerate(l):
+                for gind,g in enumerate(self.container.crys.G):
+                    if self.container.indexmap[gind][ind]==stind:
+                        lis.append(g)
+            glist.append(lis)
+        return glist
+
+    def generateJumpGroupOps(self):
+        """
+        which group operations land the first jump of a jump list to the rest of the jumps in the same
+        list.
+        """
+        glist=[]
+        for jlist in jumpnetwork:
+            tup=jlist[0]
+            for gind,g in self.container.crys.G:
