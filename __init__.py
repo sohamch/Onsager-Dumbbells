07c076b0434cfe8ddea4552d1020cce944b9c684
Soham Chattopadhyay <ch.soham@gmail.com>
2018-12-06 09:02:52 -0600

    inits for unit test

---
+++ __init__.py
@@ -0,0 +1,3 @@
+__all__ = [ "jumpnet3", "stars", "states",
+            "vector_stars", "OnsagerCalc", "PowerExpansion",
+            "collision","GFcalc_dumbbells","representations","test_structs"]
---
+++ test/__init__.py
@@ -0,0 +1,2 @@
+__all__ = [ "test_states", "test_jumpnet3", "test_representations",
+            "test_crystal_stars"]
