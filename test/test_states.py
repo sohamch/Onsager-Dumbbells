c1f26305bcbcb0c1a56f1e9d862b46f58b86a339
sohamch <ch.soham@gmail.com>
2020-09-19 07:36:35 -0500

    tested 2d dumbbell containers.

--- test/test_states.py
+++ test/test_states.py
@@ -263,3 +263,74 @@ class test_2d(test_statemaking):
 
         latt = np.array([[1., 0.], [0., 1.]])
         self.crys = crystal.Crystal(latt, [np.array([0, 0])], ["A"])
+
+    def test_dbStates(self):
+        # check that symmetry analysis is correct
+        dbstates = dbStates(self.crys, 0, self.family)
+        self.assertEqual(len(dbstates.symorlist), 1)
+        # check that every (i,or) set is accounted for
+        sm = 0
+        for i in dbstates.symorlist:
+            sm += len(i)
+        self.assertEqual(sm, len(dbstates.iorlist))
+
+        # test indexmapping
+        for gdumb in dbstates.G:
+            # First check that all states are accounted for.
+            self.assertEqual(len(gdumb.indexmap[0]), len(dbstates.iorlist))
+            for idx1, tup1 in enumerate(dbstates.iorlist):
+                i, o = tup1[0], tup1[1]
+                R, (ch, inew) = dbstates.crys.g_pos(dbstates.G_crys[gdumb], np.array([0, 0]), (dbstates.chem, i))
+                onew = np.dot(gdumb.cartrot, o)
+                count = 0
+                for idx2, tup2 in enumerate(dbstates.iorlist):
+                    if inew == tup2[0] and (np.allclose(tup2[1], onew, atol=dbstates.crys.threshold) or
+                                      np.allclose(tup2[1], -onew, atol=dbstates.crys.threshold)):
+                        count +=1
+                        self.assertEqual(gdumb.indexmap[0][idx1], idx2, msg="{}, {}".format(gdumb.indexmap[0][idx1], idx2))
+                self.assertEqual(count, 1)
+
+        # test_indexedsymlist
+        for i1, symindlist, symstatelist in zip(itertools.count(), dbstates.symIndlist, dbstates.symorlist):
+            for stind, state in zip(symindlist, symstatelist):
+                st_iorlist = dbstates.iorlist[stind]
+                self.assertEqual(st_iorlist[0], state[0])
+                self.assertTrue(np.allclose(st_iorlist[1], state[1], atol=dbstates.crys.threshold))
+
+    def test_mStates(self):
+        dbstates = dbStates(self.crys, 0, self.family)
+        mstates1 = mStates(self.crys, 0, self.family)
+
+        # check that symmetry analysis is correct
+        self.assertEqual(len(mstates1.symorlist), 1)
+
+        # check that negative orientations are accounted for
+        for i in range(len(mstates1.symorlist)):
+            self.assertEqual(len(mstates1.symorlist[i]) / len(dbstates.symorlist[i]), 2)
+
+        # check that every (i,or) set is accounted for
+        sm = 0
+        for i in mstates1.symorlist:
+            sm += len(i)
+        self.assertEqual(sm, len(mstates1.iorlist))
+
+        # check indexmapping
+        for gdumb in mstates1.G:
+            self.assertEqual(len(gdumb.indexmap[0]), len(mstates1.iorlist))
+            for stateind, tup in enumerate(mstates1.iorlist):
+                i, o = tup[0], tup[1]
+                R, (ch, inew) = mstates1.crys.g_pos(mstates1.G_crys[gdumb], np.array([0, 0]), (mstates1.chem, i))
+                onew = np.dot(gdumb.cartrot, o)
+                count = 0
+                for j, t in enumerate(mstates1.iorlist):
+                    if t[0] == inew and np.allclose(t[1], onew):
+                        foundindex = j
+                        count += 1
+                self.assertEqual(count, 1)
+                self.assertEqual(foundindex, gdumb.indexmap[0][stateind])
+
+        # Check indexing of symlist
+        for symind, symIndlist, symstlist in zip(itertools.count(), mstates1.symIndlist, mstates1.symorlist):
+            for idx, state in zip(symIndlist, symstlist):
+                self.assertEqual(mstates1.iorlist[idx][0],state[0])
+                self.assertTrue(np.allclose(mstates1.iorlist[idx][1], state[1], atol=mstates1.crys.threshold))
